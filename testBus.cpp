b59766cd98d05d6cf348b5857ea08a5dfeda88d5
Mikhail Surin <surinmike@gmail.com>
2020-04-01 03:27:49 +0300

    4000 messages

--- testBus.cpp
+++ testBus.cpp
@@ -12,10 +12,14 @@ int main() {
 
     TcpBus second(TcpBus::Options{.port = 4002, .fixed_pool_size = 2}, bufferPool, manager);
 
+    constexpr size_t messages_count = 4000;
+
     std::thread t([&] {
         BufferPool bufferPool{4098};
         EndpointManager manager;
         TcpBus first(TcpBus::Options{.port = 4001, .fixed_pool_size = 2}, bufferPool, manager);
+
+        size_t messages_received = 0;
 
         first.set_handler([&](int endp, SharedView view) {
                 Operation op2;
@@ -23,21 +27,25 @@ int main() {
                 assert(op2.key() == "key");
                 assert(op2.data() == "data");
 
-                exit(0);
+                if ((++messages_received) == messages_count) {
+                    exit(0);
+                }
             });
 
         first.loop();
     });
 
-    Operation op;
-    op.set_data("data");
-    op.set_key("key");
+    for (size_t i = 0; i < messages_count; ++i) {
+        Operation op;
+        op.set_data("data");
+        op.set_key("key");
 
-    ScopedBuffer buffer{bufferPool};
-    buffer.get().resize(op.ByteSizeLong());
-    op.SerializeToArray(buffer.get().data(), buffer.get().size());
+        ScopedBuffer buffer{bufferPool};
+        buffer.get().resize(op.ByteSizeLong());
+        op.SerializeToArray(buffer.get().data(), buffer.get().size());
 
-    second.send(manager.register_endpoint("::1", 4001), std::move(buffer));
+        second.send(manager.register_endpoint("::1", 4001), std::move(buffer));
+    }
 
     second.loop();
 
