f0381d2aedd3db03ab36dd0b5abfccddc448b5bd
Mikhail Surin <surinmike@gmail.com>
2020-04-01 04:12:47 +0300

    rust-like Mutex

--- util.h
+++ util.h
@@ -40,3 +40,51 @@ private:
     std::mutex mutex_;
     std::condition_variable cv_;
 };
+
+template<typename T>
+class ExclusiveWrapper {
+public:
+    class Guard {
+    public:
+        Guard(T& value, std::unique_lock<std::mutex> lock)
+            : value_(value)
+            , lock_(std::move(lock))
+        {
+        }
+
+        T& operator * () {
+            return value_;
+        }
+
+        const T& operator * () const {
+            return value_;
+        }
+
+        T* operator -> () {
+            return &value_;
+        }
+
+        const T* operator -> () const {
+            return &value_;
+        }
+
+    private:
+        T& value_;
+        std::unique_lock<std::mutex> lock_;
+    };
+
+public:
+    template<typename... Args>
+    ExclusiveWrapper(Args... args)
+        : value_(std::forward<Args>(args)...)
+    {
+    }
+
+    Guard get() {
+        return Guard(value_, std::unique_lock(mutex_));
+    }
+
+private:
+    T value_;
+    std::mutex mutex_;
+};
